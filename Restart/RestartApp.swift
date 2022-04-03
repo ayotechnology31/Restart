aaff2fb9a6d9f4bbb2e32bd66849f02d1c6a0cd4
Amphavanh Lithyouvong <alithyouvong@gmail.com>
2022-04-03 17:25:56 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Amphavanh Lithyouvong on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
